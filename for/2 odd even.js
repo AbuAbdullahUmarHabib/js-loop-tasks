0d60e594177bcbc34200f596ce226795197bd88c
Abu Abdullah Umar <itshabib.me+PH@gmail.com>
2026-01-27 13:26:33 +0600

    Solved 2 odd even.js

--- for/2 odd even.js
+++ for/2 odd even.js
@@ -6,7 +6,11 @@ Find all the odd numbers from 61 to 100.
 
  */
 
-
+for (let i = 61; i <= 100; i++) {
+  if (i % 2 !== 0) {
+    console.log("This is an Odd Number", i);
+  }
+}
 /***
 
 Subtask-2:
@@ -14,5 +18,9 @@ Subtask-2:
 Find all the even numbers from 78 to 98.
 
  */
-
+for (let evenNumber = 78; evenNumber <= 98; evenNumber++) {
+  if (evenNumber % 2 === 0) {
+    console.log("This is an Even Number", evenNumber);
+  }
+}
 /*programming hero*/
