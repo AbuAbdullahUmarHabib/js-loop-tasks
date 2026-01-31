5d0fae09d599b2ee8b836d34811700ee4e0eaabb
Abu Abdullah Umar <itshabib.me+PH@gmail.com>
2026-01-31 10:25:40 +0600

    3 sum of odd numbers solved

--- for/3 sum of odd numbers.js
+++ for/3 sum of odd numbers.js
@@ -1,5 +1,3 @@
-
-
 /***
 
 Subtask-1:
@@ -7,6 +5,13 @@ Subtask-1:
 Display sum of all the odd numbers from 91 to 129.
 
  */
+let oddSum = 0;
+for (let i = 91; i <= 129; i++) {
+  if (i % 2 !== 0) {
+    oddSum += i;
+  }
+}
+console.log(oddSum);
 /***
 
 Subtask-2:
@@ -14,5 +19,12 @@ Subtask-2:
 Display sum of all the even numbers from 51 to 85.
 
  */
+let evnSum = 0;
+for (let i = 51; i <= 85; i++) {
+  if (i % 2 === 0) {
+    evnSum = evnSum + i;
+  }
+}
+console.log(evnSum);
 
 /*programming hero*/
